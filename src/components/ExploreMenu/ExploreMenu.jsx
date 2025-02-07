8103fcaa64279526d2f7fd23d15b6cdc1f26b3bc
MiguelOttoDev <105008843+MiguelOttoDev@users.noreply.github.com>
2025-02-06 22:15:22 -0300

    resolvendo responsividade

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -4,8 +4,10 @@ import { menu_list } from '../../assets/frontend_assets/assets'
 const ExploreMenu = ({category, setCategory}) => {
     return (
         <div className='explore-menu' id='explore-menu'>
+            <div className="div">
             <h1>Explore nosso cardápio</h1>
             <p className="explore-menu-text">Escolha entre um menu diversificado com uma deliciosa variedade de pratos. Nossa missão é satisfazer seus desejos e elevar sua experiência gastronômica, uma refeição deliciosa de cada vez.</p>
+            </div>
             <div className="explore-menu-list">
                 {menu_list.map((item, index) => {
                     return (
